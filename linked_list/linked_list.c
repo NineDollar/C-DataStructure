e233345f48cf2d41156f2102a82b7fa0924a4fe3
1689699455@qq.com <1689699455@qq.com>
2021-04-17 21:13:28 +0800

    单链表

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -13,10 +13,10 @@
 
 #define SIZE 20
 
-typedef struct LNode {
+typedef struct LNode {  //LNode类型
     ElemType data;  //数据域
     struct LNode *next;  //指针域
-} LNode, *LinkList;
+} LNode, *LinkList;  //LNode变量
 
 /**
  * @time 2021/4/16 21:45
@@ -36,7 +36,7 @@ Status ListInsert_L(LinkList l, int i, ElemType e) {
     if (!p || j > i - 1) {
         exit OVERFLOW;
     }
-    s = (LinkList) malloc(sizeof(struct LNode));
+    s = (LinkList) malloc(sizeof(LNode)); //LNode类型名
     s->data = e;
     s->next = l->next;
     l->next = s;
@@ -56,7 +56,7 @@ Status TraverElem(LinkList l) {
         printf("%d->", p->data);
         p = p->next;
     }
-    printf("NULL");
+    printf("NULL\n");
     return OK;
 }
 
@@ -68,11 +68,11 @@ Status TraverElem(LinkList l) {
  */
 Status InitElem(LinkList l) {
     int i, e;
-    char se[SIZE]="";
+    char se[SIZE] = "";
     i = 0;
     LinkList p, q;
     while (1) {
-        printf("请输入单链x表第%d个值（“？结束输入”）：", i++);
+        printf("请输入单链x表第%d个值（“？”结束输入）：", i++);
         fflush(stdout);
         scanf("%s", se);
         e = atoi(se);
@@ -80,7 +80,7 @@ Status InitElem(LinkList l) {
             printf("输入结束\n");
             break;
         }
-        p = (LinkList) malloc(sizeof(LinkList));
+        p = (LinkList) malloc(sizeof(LNode));
         p->data = e;
         p->next = l->next;
         l->next = p;
@@ -89,13 +89,121 @@ Status InitElem(LinkList l) {
     return OK;
 }
 
+/**
+ * @time 2021/4/17 17:45
+ * @description 获取第i个元素的值
+ * @param  i：位序
+ * @return
+ */
+Status GetIndexElem(LinkList l, int i, int *e) {
+    LinkList p;
+    int j;
+    p = l->next;
+    j = 1;
+    while (p && j < i) {
+        p = p->next;
+        j++;
+    }
+    if (!p || j > i) {
+        return ERROR;
+    }
+    *e = p->data;
+    return OK;
+}
+
+/**
+ * @time 2021/4/17 17:52
+ * @description 删除位序i的元素
+ * @param  i:位序
+ * @return
+ */
+Status ListDelete_L(LinkList l, int i, int *e) {
+    LinkList p, q;
+    int j = 0;
+    p = l;
+    while (p->next && j < i - 1) {  //找到删除元素上一个节点
+        p = p->next;
+        j++;
+    }
+    if (!(p->next) || j > i - 1) {
+        return ERROR;
+    }
+    q = p->next; //删除的节点
+    p->next = q->next;
+    *e = q->data;
+    free(q);
+    return OK;
+}
+
+/**
+ * @time 2021/4/17 20:28
+ * @description 两非递减单链表归并
+ * @param  *lc:二级指针,lc本身是指针,需要返回指针,所以再取一次指针,指针的指针,也就是地址的地址
+ * @return
+ */
+Status MergeList(LinkList l, LinkList lb, LinkList *lc) {  //*lc二级指针
+    LinkList pa, pb, pc;
+    pa = l->next;
+    pb = lb->next;
+    pc = l;
+    *lc = pc;  //保存pc的首地址
+    while (pa && pb) {
+        if (pa->data <= pb->data) {
+            pc->next = pa;
+            pc = pa;
+            pa = pa->next;
+        } else {
+            pc->next = pb;
+            pc = pb;
+            pb = pb->next;
+        }
+    }
+    pc->next = pa ? pa : pb;
+    free(lb);
+    return OK;
+}
+
 int main() {
     LinkList l;
     l = (LinkList) malloc(sizeof(struct LNode));
     l->next = NULL;
-    InitElem(l);
-//    ListInsert_L(l, 0, 10);
+
+//    输入值
+//    InitElem(l);
+
+    ListInsert_L(l, 4, 345);
+    ListInsert_L(l, 3, 267);
+    ListInsert_L(l, 1, 120);
+    ListInsert_L(l, 0, 10);
     TraverElem(l);
+    printf("-----\n");
+
+//    获取第i个元素
+    /*int e,index;
+    index = 2;
+    GetIndexElem(l, index, &e);
+    printf("位序%d的值：%d\n", index, e);*/
+
+//    删除
+    /*int e,index;
+    index = 2;
+    ListDelete_L(l, index, &e);
+    printf("删除位序%d的值:%d\n", index, e);
+    TraverElem(l);*/
+
+//    归并
+    /*LinkList lb, lc;
+    lb = (LinkList) malloc(sizeof(LNode));
+    lb->next = NULL;
+    ListInsert_L(lb, 4, 789);
+    ListInsert_L(lb, 3, 325);
+    ListInsert_L(lb, 1, 69);
+    ListInsert_L(lb, 0, 45);
+    TraverElem(lb);
+    printf("-----\n");
+
+    MergeList(l, lb, &lc);
+    TraverElem(lc);*/
 
     return TRUE;
 }
